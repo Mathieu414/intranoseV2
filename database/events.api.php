7630a54274f72c80115c6f9fc0e60cde0ae7aa55
mathieu <mathieu.perrin@insa-lyon.fr>
2023-01-08 13:15:01 +0100

    submit button and api function

--- database/events.api.php
+++ database/events.api.php
@@ -54,7 +54,7 @@ function get_event_by_id($event_id, $user_id = null)
 function get_competitions_by_event_id($event_id, $user_id = null)
 {
     return fetch(
-        "SELECT courses.*, inscriptions_courses.present as present FROM courses 
+        "SELECT courses.*, inscriptions_courses.present  as present, inscriptions_courses.rmq as rmq FROM courses 
         LEFT JOIN inscriptions_courses 
             ON inscriptions_courses.id_course = courses.cid 
             AND inscriptions_courses.id_runner = ?
@@ -108,18 +108,88 @@ function publish_event($event_id, $state)
     return query_db("UPDATE deplacements SET open=? WHERE did=? LIMIT 1", $state, $event_id);
 }
 
-function save_registration($event_id, $user_id, $get)
+function save_registration($event_id, $user_id, $post, $competitions_id)
 {
-    if (isset($get['submit'])) {
-        if (isset($get['event_entry'])) {
-            if (isset($get['event_transport'])) {
-                #do the query for transport
+    if (isset($post['submit'])) {
+        $date = date('Y-m-d H:i:s', time());
+        #First check if the user wants to register to the event
+        if (isset($post['event_entry'])) {
+            query_db("REPLACE INTO inscriptions_depl(id_depl, id_runner, present, transport, heberg, courses, date, comment)
+                VALUES(?, ?, 1, 0, 0, 0, ?, '');",
+                $event_id,
+                $user_id,
+                $date
+
+            );
+            #check the transportation
+            if (isset($post['event_transport'])) {
+                query_db("UPDATE inscriptions_depl 
+                SET transport=1 
+                WHERE id_depl=? AND id_runner=?",
+                    $event_id,
+                    $user_id
+                );
             }
-            if (isset($get['event_accomodation'])) {
-                #query for accomodation
+            #check the accomodation
+            if (isset($post['event_accomodation'])) {
+                query_db("UPDATE inscriptions_depl 
+                SET heberg=1 
+                WHERE id_depl=? AND id_runner=?",
+                    $event_id,
+                    $user_id
+                );
             }
-            if (strlen($get['event_comments']) != 0) {
-                return $get;
+            #check the comments
+            if (strlen($post['event_comments']) != 0) {
+                query_db("UPDATE inscriptions_depl 
+                SET comment=? 
+                WHERE id_depl=? AND id_runner=?",
+                    $post['event_comments'],
+                    $event_id,
+                    $user_id
+                );
+            }
+            $keys = array_keys($post);
+            $courses_form = preg_grep("/course/", $keys);
+            $comment_form = preg_grep("/compet_comment_/", $keys);
+            $comment_form_keys = array_keys($comment_form);
+            #Handle the comments first, create db entries for each comments
+            foreach ($comment_form_keys as $key => $value) {
+                query_db(
+                    "REPLACE INTO inscriptions_courses(id_course,id_runner,id_cat,licence,si,present,surclasse,rmq)
+            VALUES(?,?, 0, 0, 0,0,0,?)",
+                    $competitions_id[$key],
+                    $user_id,
+                    $post[$comment_form[$value]]
+                );
+            }
+            #Register for each course entry
+            foreach ($courses_form as $course) {
+                query_db(
+                    "UPDATE inscriptions_courses 
+                    SET present = 1 
+                    WHERE id_course = ? AND id_runner = ?",
+                    intval($post[$course]),
+                    $user_id
+                );
+            }
+            return $comment_form;
+        } else {
+            #unregister to the event and to the competitions
+            query_db("REPLACE INTO inscriptions_depl(id_depl, id_runner, present, transport, heberg, courses, date, comment)
+                VALUES(?, ?, 0, 0, 0, 0, ?, '');",
+                $event_id,
+                $user_id,
+                $date
+
+            );
+            foreach ($competitions_id as $value) {
+                query_db(
+                    "REPLACE INTO inscriptions_courses(id_course,id_runner,id_cat,licence,si,present,surclasse,rmq)
+            VALUES(?,?, 0, 0, 0,0,0,'')",
+                    $value,
+                    $user_id
+                );
             }
         }
 
